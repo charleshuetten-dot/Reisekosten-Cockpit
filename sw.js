9e28855e99ec6113ec6bd0a8202369c6adc01d87
charleshuetten-dot <charleshuetten@gmail.com>
2026-07-22 17:16:14 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
    Hinweis: Kommentare bewusst ohne Umlaute - Cloudflare liefert .js ohne
    charset-Angabe aus, dann stellt der Browser Umlaute falsch dar.
 */
-const CACHE = 'dozenten-cockpit-v37';
+const CACHE = 'dozenten-cockpit-v38';
 const SHELL = ['/', '/index.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
